0acd0b82aee869051bc6156f72d03af33e9aba40
Apprentice <apprentice@dbc-chicago-009.local>
2013-09-20 21:07:03 -0500

    created new user successfully

--- app/controllers/login.rb
+++ app/controllers/login.rb
@@ -1,21 +1,23 @@
+post '/logout' do
+  session.clear
+  redirect '/'
+
+end 
+
 post '/login' do
-  # user hash is sent 
   user = User.find_by_email(params[:user][:email]).try(:authenticate, params[:user][:password])
-  # create user and save it 
   if user
     session[:user_id] = user.id 
   else
     session[:error] = 'invalid login'    # should we use a session value instead? 
     redirect '/'
-  end 
-  # turn session on 
-
+  end
   redirect '/'
 end
 
 post '/signup' do
-
-
+  user = User.create(params[:user])
+  user.save
+  session[:user_id] = user.id
   erb :index
-
 end
