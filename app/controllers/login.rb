1ad6699db4351bcdf28667eb67f7ad1fc6538727
Apprentice <apprentice@dbc-chicago-009.local>
2013-09-20 20:46:55 -0500

    functioning login'

--- app/controllers/login.rb
+++ app/controllers/login.rb
@@ -1,7 +1,16 @@
 post '/login' do
+  # user hash is sent 
+  user = User.find_by_email(params[:user][:email]).try(:authenticate, params[:user][:password])
+  # create user and save it 
+  if user
+    session[:user_id] = user.id 
+  else
+    session[:error] = 'invalid login'    # should we use a session value instead? 
+    redirect '/'
+  end 
+  # turn session on 
 
-
-  erb :index
+  redirect '/'
 end
 
 post '/signup' do
