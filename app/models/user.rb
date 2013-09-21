29d81d30ad1c787bdbd2b9b7ce783bd8adf33811
Apprentice <apprentice@dbc-chicago-009.local>
2013-09-21 18:31:46 -0500

    modified user.rb & index.erb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,16 @@
 class User < ActiveRecord::Base
   require 'bcrypt'
   has_secure_password
-  has_many :games 
+  has_many :games
+  has_many :responses, through: :games
+
+
+  def number_of_correct_guesses
+    self.responses.where("correct = true").count
+  end 
+
+  def number_of_incorrect_guesses
+    self.responses.where("correct = false").count
+  end 
 
 end
